9761a81f8e05f52216e1619f7715fd547af631e1
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-18 16:03:27 -0400

    change sh

--- child-processes/install_cdo.sh
+++ child-processes/install_cdo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-home=`pwd`/child-processes/cdo-1.9.1
+home=`pwd`/cdo-1.9.1
 cd $home
 ./configure --with-netcdf=/usr -prefix=$home
-make && make check && make install
+make
+make check
+make install
