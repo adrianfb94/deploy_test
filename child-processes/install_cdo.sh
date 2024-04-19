a52af8be57b97780148d6ab147e989c948286a89
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-19 11:42:24 -0400

    netcdf

--- child-processes/install_cdo.sh
+++ child-processes/install_cdo.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 home=`pwd`
-# mkdir -p $home/cdo_installed
-
-
-cd $home/cdo-1.9.1
-# ./configure --with-netcdf=/usr -prefix=$home/cdo_installed
-make
-# make check
-# make install
-
 
 # cd $home/zlib-1.2.8
 # ./configure -prefix=$home/cdo_installed
@@ -25,36 +16,21 @@ make
 # make install
 
 
-# #   download, compile and install --> netCDF
-# cd $home
-# wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.5.0.tar.gz
-# tar -xzvf netcdf-4.5.0.tar.gz
-# cd netcdf-4.5.0/
-# CPPFLAGS=-I~/cdo_install/include LDFLAGS=-L~/cdo_install/lib ./configure -prefix=~/cdo_install CFLAGS=-fPIC
-# make && make check && make install
+cd $home/netcdf-c-4.5.0
+CPPFLAGS=-I$home/cdo_installed/include LDFLAGS=-L$home/cdo_installed/lib ./configure -prefix=$home/cdo_installed CFLAGS=-fPIC
+make && make check && make install
 
-# #   download, compile and install --> jasper
-# cd $home
-# wget http://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
-# unzip jasper-1.900.1.zip
-# cd jasper-1.900.1
-# ./configure -prefix=~/cdo_install CFLAGS=-fPIC
-# make && make check && make install
+cd $home/jasper-1.900.1
+./configure -prefix=$home/cdo_installed CFLAGS=-fPIC
+make && make check && make install
 
-# #   download, compile and install --> grib_api
-# cd $home
-# wget https://software.ecmwf.int/wiki/download/attachments/3473437/grib_api-1.24.0-Source.tar.gz?api=v2 -O grib_api-1.24.0.tar.gz
-# tar -xzvf grib_api-1.24.0.tar.gz
-# cd grib_api-1.24.0-Source
-# ./configure -prefix=~/cdo_install CFLAGS=-fPIC -with-netcdf=~/cdo_install -with-jasper=~/cdo_install
-# make && make check && make install
+cd $home/grib_api-1.24.0-Source
+./configure -prefix=$home/cdo_installed CFLAGS=-fPIC -with-netcdf=$home/cdo_installed -with-jasper=$home/cdo_installed
+make && make check && make install
 
-# #   download, compile and install --> cdo
-# cd $home
-# wget https://code.mpimet.mpg.de/attachments/download/15653/cdo-1.9.1.tar.gz
-# tar -xvzf cdo-1.9.1.tar.gz
-# cd cdo-1.9.1
-# ./configure -prefix=~/cdo_install CFLAGS=-fPIC -with-netcdf=~/cdo_install -with-jasper=~/cdo_install -with-hdf5=~/cdo_install -with-grib_api=~/cdo_install/grib_api-1.24.0-Source
+
+# cd $home/cdo-1.9.1
+# ./configure -prefix=$home/cdo_installed CFLAGS=-fPIC -with-netcdf=$home/cdo_installed -with-jasper=$home/cdo_installed -with-hdf5=$home/cdo_installed -with-grib_api=$home/cdo_installed/grib_api-1.24.0-Source
 # make && make check && make install
 
 # #   set PATH
