847a5067e2fc044bb5dd6b21f92ed902de4ae40c
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-21 02:31:01 -0400

    make -j 1

--- child-processes/install_cdo.sh
+++ child-processes/install_cdo.sh
@@ -7,4 +7,5 @@ path_cdo=$home/cdo_v1.9
 tar -xzvf cdo-1.9.1.tar.gz
 cd $home/cdo-1.9.1
 ./configure --enable-netcdf4  --enable-zlib --prefix=$path_cdo --with-netcdf=/usr
-make && make install
+make -j 1
+make install
