109fd83911fcad19cb3e64e1fe3c9ff1f3950d39
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-21 02:04:03 -0400

    quit check

--- child-processes/install_cdo.sh
+++ child-processes/install_cdo.sh
@@ -7,4 +7,4 @@ path_cdo=$home/cdo_v1.9
 tar -xzvf cdo-1.9.1.tar.gz
 cd $home/cdo-1.9.1
 ./configure --enable-netcdf4  --enable-zlib --prefix=$path_cdo --with-netcdf=/usr
-make && make check && make install
+make && make install
