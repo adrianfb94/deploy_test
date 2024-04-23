30c58188c3d2711cdaf4dcfe4dc7239db72a7fc3
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-23 15:05:42 -0400

    removed folders

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -17,6 +17,11 @@ ls
 
 rm $home/cdo_dependecies.tar.gz
 
+rm -r $path_dep
+rm -r $path_exe
+rm -r $path_cdo
+
+
 # ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep CFLAGS=-I$path_dep/include
 # make
 # make install
