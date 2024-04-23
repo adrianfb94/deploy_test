7efb35fe848cf686699f0d39c2d542d3830dba0f
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-23 15:36:59 -0400

    test configure

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -6,7 +6,7 @@ home=`pwd`
 
 cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
 
-tar -xzvf $home/cdo_dependecies.tar.gz
+tar -xzf $home/cdo_dependecies.tar.gz
 
 path_dep=$home/cdo_full_disable-shared
 path_exe=$home/cdo_exe
@@ -17,13 +17,12 @@ echo $path_dep
 echo $path_exe
 echo $path_cdo
 
+cd $path_cdo
+./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep CFLAGS=-I$path_dep/include
+# make
+# make install
+
 rm $home/cdo_dependecies.tar.gz
 rm -r $path_dep
 rm -r $path_exe
 rm -r $path_cdo
-
-# cd $path_cdo
-# ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep CFLAGS=-I$path_dep/include
-# make
-# make install
-
