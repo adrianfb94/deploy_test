a43a217d22bbc4fcc699099a6574a1f8168f49b0
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-23 14:51:26 -0400

    added sh

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -8,12 +8,13 @@ cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
 
 tar -xzvf $home/cdo_dependecies.tar.gz
 
-path_cdo_dep=$home/cdo_full_disable-shared
-path_cdo_exe=$home/cdo_exe
+path_dep=$home/cdo_full_disable-shared
+path_exe=$home/cdo_exe
 path_cdo=$home/cdo-1.9.1
 
 cd $path_cdo
-./configure --enable-netcdf4 --enable-zlib --prefix=$path_cdo_exe --with-netcdf=$path_cdo_dep --with-hdf5=$path_cdo_dep CFLAGS=-I$path_cdo_dep/include
+ls
+# ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep CFLAGS=-I$path_dep/include
 # make
 # make install
 
