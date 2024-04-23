d0647b71c32710407845e96752455ffbffbad62b
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-23 15:26:30 -0400

    removed file

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -12,16 +12,17 @@ path_dep=$home/cdo_full_disable-shared
 path_exe=$home/cdo_exe
 path_cdo=$home/cdo-1.9.1
 
-cd $path_cdo
-ls
+
+echo $path_dep
+echo $path_exe
+echo $path_cdo
 
 rm $home/cdo_dependecies.tar.gz
-
-rm -r $path_dep
-rm -r $path_exe
+rm -r $path_cdo_dep
+rm -r $path_cdo_exe
 rm -r $path_cdo
 
-
+# cd $path_cdo
 # ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep CFLAGS=-I$path_dep/include
 # make
 # make install
