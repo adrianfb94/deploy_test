806e361a9d7f61b1bd99ccdafd4d9b019c828a31
Adrian <56260967+adrianfb94@users.noreply.github.com>
2024-04-25 15:02:58 -0400

    Update install.sh

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-home=`pwd`
 
-# split -b 1M cdo_dependecies.tar.gz cdo_dependecies.tar.gz.part
-
-cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
-
-tar -xzf $home/cdo_dependecies.tar.gz
-
-path_dep=$home/cdo_full_disable-shared
-path_exe=$home/cdo_exe
-path_cdo=$home/cdo-1.9.1
+rm cdo.log
+python find_function.py >> cdo.log
 
 
-cd $path_cdo
-CPPFLAGS=-I$path_dep/include LDFLAGS=-L$path_dep/lib CFLAGS=-I$path_dep/include ./configure --prefix=$path_exe --with-netcdf=$path_dep # --with-hdf5=$path_dep
-# ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep
+# home=`pwd`
 
-# make
-# make install
+# # split -b 1M cdo_dependecies.tar.gz cdo_dependecies.tar.gz.part
 
-rm $home/cdo_dependecies.tar.gz
-rm -r $path_dep
-rm -r $path_exe
-rm -r $path_cdo
+# cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
+
+# tar -xzf $home/cdo_dependecies.tar.gz
+
+# path_dep=$home/cdo_full_disable-shared
+# path_exe=$home/cdo_exe
+# path_cdo=$home/cdo-1.9.1
+
+
+# cd $path_cdo
+# CPPFLAGS=-I$path_dep/include LDFLAGS=-L$path_dep/lib CFLAGS=-I$path_dep/include ./configure --prefix=$path_exe --with-netcdf=$path_dep # --with-hdf5=$path_dep
+# # ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep
+
+# # make
+# # make install
+
+# rm $home/cdo_dependecies.tar.gz
+# rm -r $path_dep
+# rm -r $path_exe
+# rm -r $path_cdo
