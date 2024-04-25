85eb6eb630c50b9572bd680ae34a6cbe3d2b9587
Adrian <56260967+adrianfb94@users.noreply.github.com>
2024-04-25 17:06:59 -0400

    export on install.sh

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -2,8 +2,6 @@
 
 home=`pwd`
 
-
-
 # split -b 1M cdo_dependecies.tar.gz cdo_dependecies.tar.gz.part
 
 cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
@@ -16,9 +14,14 @@ path_exe=$home/cdo_exe
 path_cdo=$home/cdo-1.9.1
 
 
+export CPPFLAGS=-I$path_dep/include 
+export LDFLAGS=-L$path_dep/lib 
+export CFLAGS=-I$path_dep/include
+
+
 cd $path_cdo
 # CPPFLAGS=-I$path_dep/include LDFLAGS=-L$path_dep/lib CFLAGS=-I$path_dep/include ./configure --prefix=$path_exe --with-netcdf=$path_dep # --with-hdf5=$path_dep
-./configure --prefix=$path_exe --with-netcdf=$path_dep # --with-hdf5=$path_dep
+./configure --prefix=$path_exe --enable-netcdf4 --with-netcdf=$path_dep --with-hdf5=$path_dep
 # ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep
 
 # make
