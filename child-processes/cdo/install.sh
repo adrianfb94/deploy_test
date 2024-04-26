5661806e21f8a6cd33ce7115390290221aeb5594
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-26 04:21:35 -0400

    zlib

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -5,26 +5,14 @@
 # /opt/render/project/src/child-processes/cdo/cdo-1.9.1/libcdi/app
 
 home=`pwd`
+mkdir cdo_exe
 
-path_cdo_opt=/opt/render/project/src/child-processes/cdo
-
-
-# cp $home/gdal-3.4.0.tar.gz $path_cdo_opt/
-# cd $path_cdo_opt
-# tar -xzf gdal-3.4.0.tar.gz
-# cd gdal-3.4.0
-# ./configure --prefix=$path_cdo_opt
-# make
-# make install
- 
-
-# cp $home/zlib-1.2.12.tar.gz $path_cdo_opt/
-# cd $path_cdo_opt
-# tar -xzf zlib-1.2.12.tar.gz
-# cd zlib-1.2.12
-# ./configure --prefix=$path_cdo_opt/cdo_exe  --static
-# make
-# make install
+cd $home
+tar -xzf zlib-1.2.12.tar.gz
+cd zlib-1.2.12
+./configure --prefix=$home/cdo_exe
+make
+make install
 
 
 # cp $home/hdf5-1.14.3.tar.gz $path_cdo_opt/
@@ -44,10 +32,10 @@ path_cdo_opt=/opt/render/project/src/child-processes/cdo
 # make
 # make install
 
-cp $home/cdo-1.9.1.tar.gz $path_cdo_opt/
-cd $path_cdo_opt
-tar -xzf cdo-1.9.1.tar.gz
-cd cdo-1.9.1
-./configure --enable-netcdf4 --enable-zlib --prefix=$path_cdo_opt/cdo_exe --with-netcdf=$path_cdo_opt --with-hdf5=$path_cdo_opt CFLAGS=-I$path_cdo_opt/include --disable-shared
-make
-make install
+# cp $home/cdo-1.9.1.tar.gz $path_cdo_opt/
+# cd $path_cdo_opt
+# tar -xzf cdo-1.9.1.tar.gz
+# cd cdo-1.9.1
+# ./configure --enable-netcdf4 --enable-zlib --prefix=$path_cdo_opt/cdo_exe --with-netcdf=$path_cdo_opt --with-hdf5=$path_cdo_opt CFLAGS=-I$path_cdo_opt/include --disable-shared
+# make
+# make install
