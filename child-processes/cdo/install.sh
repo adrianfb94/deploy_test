31843ec668789fc317f0086b9261eaae29e40126
Adrian <56260967+adrianfb94@users.noreply.github.com>
2024-04-26 11:04:18 -0400

    cdo

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -25,20 +25,20 @@ home=`pwd`
 # rm -r $home/hdf5-1.14.3
 
 
-cd $home
-tar -xzf netcdf-c-4.9.2.tar.gz
-cd Unidata-netcdf-c-d4145f3
-./configure --prefix=$home/cdo_exe --disable-hdf5
-make
-make install
-
-rm -r $home/Unidata-netcdf-c-d4145f3
-
-
-
 # cd $home
-# tar -xzf cdo-1.9.1.tar.gz
-# cd cdo-1.9.1
-# ./configure --enable-netcdf4 --enable-zlib --prefix=$home/cdo_exe --with-netcdf=$home/cdo_exe --with-hdf5=$home/cdo_exe
+# tar -xzf netcdf-c-4.9.2.tar.gz
+# cd Unidata-netcdf-c-d4145f3
+# ./configure --prefix=$home/cdo_exe --disable-hdf5
 # make
 # make install
+
+# rm -r $home/Unidata-netcdf-c-d4145f3
+
+
+
+cd $home
+tar -xzf cdo-1.9.1.tar.gz
+cd cdo-1.9.1
+./configure --enable-netcdf4 --enable-zlib --prefix=$home/cdo_exe --with-netcdf=$home/cdo_exe
+make
+make install
