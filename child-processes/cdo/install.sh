3946e5fbe9a0e7035ace4fcda578008143c65634
adrianfb94 <adrianfuentesbarrios@gmail.com>
2024-04-26 00:19:46 -0400

    changes

--- child-processes/cdo/install.sh
+++ child-processes/cdo/install.sh
@@ -1,16 +1,17 @@
+
 #!/bin/bash
 
 home=`pwd`
 
-# split -b 1M cdo_dependecies.tar.gz cdo_dependecies.tar.gz.part
+# # split -b 1M cdo_dependecies.tar.gz cdo_dependecies.tar.gz.part
 
-cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
+# cat cdo_dependecies.tar.gz.part* >> cdo_dependecies.tar.gz
 
-tar -xzf $home/cdo_dependecies.tar.gz
+# tar -xzf $home/cdo_dependecies.tar.gz
 
-path_dep=$home/cdo_full_disable-shared
-path_exe=$home/cdo_exe
-path_cdo=$home/cdo-1.9.1
+# path_dep=$home/cdo_full_disable-shared
+# path_exe=$home/cdo_exe
+# path_cdo=$home/cdo-1.9.1
 
 
 # # export CPPFLAGS=-I$path_dep/include 
@@ -18,24 +19,23 @@ path_cdo=$home/cdo-1.9.1
 # # export CFLAGS=-I$path_dep/include
 
 
-# cd $path_cdo
-# # CPPFLAGS=-I$path_dep/include LDFLAGS=-L$path_dep/lib CFLAGS=-I$path_dep/include ./configure --prefix=$path_exe --with-netcdf=$path_dep # --with-hdf5=$path_dep
-# ./configure --prefix=$path_exe --enable-netcdf4 
-# # ./configure --enable-netcdf4 --enable-zlib --prefix=$path_exe --with-netcdf=$path_dep --with-hdf5=$path_dep
-
-# # make
-# # make install
-
-
-tar -xzf $home/netcdf-c-4.9.2.tar.gz
-cd $home/Unidata-netcdf-c-d4145f3
-./configure --prefix=$path_exe --enablle-netcdf4 --disable-hdf5 # --disable-shared 
+# tar -xzf $home/netcdf-c-4.9.2.tar.gz
+# cd $home/Unidata-netcdf-c-d4145f3
+# ./configure --prefix=$path_exe --enable-netcdf4 --disable-hdf5 # --disable-shared 
 # make
 # make install
 
-rm $home/cdo_dependecies.tar.gz
-rm -r $home/Unidata-netcdf-c-d4145f3
-rm -r $path_dep
-# rm -r $path_exe
-rm -r $path_cdo
+# cd $home
+# tar -xzf $home/cdo-1.9.1.tar.gz
+cd $home/cdo-1.9.1
+# ./configure --prefix=$home/cdo_exe --enable-netcdf4 --with-netcdf=$home/cdo_exe
+# make
+make install
 
+# cd $home
+
+# rm $home/cdo_dependecies.tar.gz
+# rm -r $home/Unidata-netcdf-c-d4145f3
+# rm -r $path_dep
+# rm -r $path_exe
+# rm -r $path_cdo
